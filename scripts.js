32a3b41c4fe4087bc162bb6490c7f2e099b11d90
Brittany Thomas <yaneelthomas162@gmail.com>
2023-11-05 12:38:31 -0500

    Updates to CSS and JavaScript code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var form = document.querySelector("form");
+    var message = document.querySelector(".message");
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        let emailInput = document.querySelector("#email");
+        let email = emailInput.value.trim();
+
+        if (email === "" || !emailRegex.test(email)) {
+            message.textContent = "Please enter a valid email address."
+        } else {
+            message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }
+    })
+
+});
+
+   
